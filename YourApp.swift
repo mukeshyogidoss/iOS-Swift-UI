88dd11eccc11a15619b9ac6a64c791e25630345a
mukeshy148 <mukesh148y@gmail.com>
2025-11-20 16:06:28 +0530

    Initial commit

---
+++ YourApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct SwiftPracticeApp: App {   // <-- This is the entry point
+    var body: some Scene {
+        WindowGroup {
+            ContentView()       // your main SwiftUI view
+        }
+    }
+}
+
